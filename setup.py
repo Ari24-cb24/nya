a6a7eb7777d862125fb377b372cd4eb3f52b7a0c
Ari <56089155+Ari24-cb24@users.noreply.github.com>
2021-04-12 00:45:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'nya',
+  packages = ['nya'],
+  version = '0.1',
+  license='MIT',
+  description = 'Nya!',
+  author = 'Ari-sama',
+  author_email = 'ari.publicmail@gmail.com',
+  url = 'https://github.com/Ari24-cb24/nya',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['anime', 'neko', 'nya'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+)
